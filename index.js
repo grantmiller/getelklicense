bdc3e9ad4c0df8d854f8a6d46ab62b3fe1e0f0b3
grantmiller <grantlmiller@gmail.com>
2015-05-18 16:49:12 -0700

    switched to standard hello world

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.listen(process.new.PORT || 3000);
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});

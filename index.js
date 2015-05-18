598734681cfba5995c328f6c85268071fa171aa5
grantmiller <grantlmiller@gmail.com>
2015-05-18 16:44:03 -0700

    included dep

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send("hello world");
+  res.send('hello world');
 });
 
 app.listen(process.new.PORT || 3000);

90cb704d1332b7cde694cb170bb73823b1a8ffb1
grantmiller <grantlmiller@gmail.com>
2015-05-19 09:13:57 -0700

    added api call to vendor

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 var express = require('express');
+var https = require('https');
 var app = express();
 
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('hello');
+
+var data = {
+  'assignee': "new co",
+  'app_id': "849743531a7a758b1289f61866664ac5",
+  'channel_id': "d6300a4b6d6f734354fbd38d8af8ec0d",
+  'expiration_date': "2015-06-30"
+};
+
+var options = {
+  host: 'api.staging.replicated.com',
+  port: 443,
+  path: '/vendor/v1/license',
+  method: 'POST',
+  headers: {
+    "Authorization": "ODk3ZWZiODctMGY4Yy00ZWU4LTliZTktNzRkZGM1OWRlMGZm"
+  },
+  contentType: 'application/json; charset=utf-8',
+  data: data
+};
+
+var req = https.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+// write data to request body
+req.write('data\n');
+req.write('data\n');
+req.end();
 });
 
 var port = process.env.PORT || 5000;
